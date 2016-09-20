ac705c1a4640f56b2a262206e12a09786e572bbc
weixian.lin <weixian.lin@shuyun.com>
2016-09-20 11:29:36 +0800

    start

---
+++ server.js
@@ -0,0 +1,31 @@
+const http = require("http");
+const exec = require("exec");
+
+const PORT = 9988;
+const PATH = "./html";
+
+const deployServer = http.createServer((request, response) => {
+	if(request.url.search(/deploy\/?$/i) > 0) {
+		let commands = [
+			"cd " + PATH,
+			"git pull"
+		].join(" && ");
+
+		exec(commands, (err, out, code) => {
+			if(err instanceof Error) {
+				response.writeHead(500);
+				response.end("Server Internal Error.");
+				throw err;
+			}
+			process.stderr.write(err);
+			process.stdout.write(out);
+			response.writeHead(200);
+			response.end("Deploy Done");
+		});
+	} else {
+		response.writeHead(404);
+		response.end("Not Found.");
+	}
+});
+
+deployServer.listen(PORT);
